e0f87cc476de6946347197ebb7f85df58d4c19f5
Diego Eiras <eiras.lucio@gmail.com>
2025-03-10 17:50:20 -0300

    feat: Cores, Tipografia e Ícones

--- scripts.js
+++ scripts.js
@@ -1,49 +1,16 @@
-// objeto do usuário
-const usuario = { nome: "Raphael", matricula: "123456", pendencia: false, acessibilidade: true };
+theme = localStorage.getItem('theme') || 'light-theme';
 
-// lista objetos de armários
-const armarios = [
-  { id: 1, formato: "padrao", status: true, acessivel: false },
-  { id: 2, formato: "padrao", status: true, acessivel: false },
-  { id: 3, formato: "padrao", status: true, acessivel: false },
-  { id: 4, formato: "padrao", status: false, acessivel: true },
-  { id: 5, formato: "padrao", status: false, acessivel: true },
-  { id: 6, formato: "duplo", status: true, acessivel: true },
-  { id: 7, formato: "duplo", status: false, acessivel: true },
-  { id: 8, formato: "duplo", status: false, acessivel: true },  
-];
-
-// função para reserva do armário, incluindo as regras.
-function reservarArmario() {
+function toogleTheme() {
+  themeToogleIcon = document.querySelector('.themeToogle span')
   
-  // obter tipo de armário selecionado pelo usuário no html.
-  let tipoSelecionado = document.getElementById("tipoArmario").value;
-  
-  // na lista, filtrar apenas os armários que estão disponíveis e que são acessiveis ao usuário.
-  let armariosDisponiveis = armarios.filter(a => a.formato === tipoSelecionado && a.status === true && usuario.acessibilidade === a.acessivel);
-  
-  // caso não exista armário disponível, retorna para o usuário mensagem.
-  if (armariosDisponiveis.length === 0) {
-    document.getElementById("resultado").innerText = `Olá, ${usuario.nome}! Nenhum armário disponível para o tipo selecionado.`;
-    return;
+  if (theme === 'light-theme') {
+    themeToogleIcon.innerText = 'dark_mode'
+    theme = 'dark-theme'
+  } else {
+    themeToogleIcon.innerText = 'light_mode'
+    theme = 'light-theme'
   }
-  
-  // Caso exista armário(s) disponível, seguimos sorteando uma opção. 
-  let armarioSorteado = armariosDisponiveis[Math.floor(Math.random() * armariosDisponiveis.length)];
-  
-  // Depois localizamos o armário emprestado na lista de armarios e mudamos o status do armário.
-  let armarioEmprestado = armarios.find(armario => armario.id === armarioSorteado.id);
-  armarioEmprestado.status = false;
 
-  armarioEmprestado.dataReserva = new Date();
-  armarioEmprestado.dataEntrega = new Date(armarioEmprestado.dataReserva.getTime() + 24 * 60 * 60 * 1000);
-  
-  // Finalmente, mudamos a pendencia do usuário para verdadeira.
-  usuario.pendencia = true;
-  
-  // Impmimimos uma mensagem de reserva para o usuário.
-  document.getElementById("resultado").innerText = `Olá, ${usuario.nome}! O armário ${armarioEmprestado.id} foi reservado com sucesso! Você tem até a data ${armarioEmprestado.dataEntrega.toLocaleString()} para utilizá-lo!`;
-
-  console.log(usuario);
-  console.log(armarios);
+  document.body.id = theme
+  localStorage.setItem('theme', theme)
 }
